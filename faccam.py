07409dd713ccee55db3c56567f2f3373a214093c
khalil <khalillakhdharatc@gmail.com>
2022-05-06 10:18:05 +0100

    Update faccam.py

--- faccam.py
+++ faccam.py
@@ -171,11 +171,11 @@ def show_prediction_labels_on_image(frame, predictions):
         if name!='inconnu':
             print(name)
             
-            #webbrowser.open('http://google.com')
-            #sleep(2) 
+            webbrowser.open('http://localhost:4200/account/login?user=khalil')
+            sleep(2) 
             # Go to example.com
-            #cv2.destroyAllWindows()
-            #exit(0)
+            cv2.destroyAllWindows()
+            exit(0)
 
 
 
